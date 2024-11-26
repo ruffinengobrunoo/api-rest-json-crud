f4d645f0184728ffb9b739d44a2eeb3151c5129d
ruffinengobrunoo <ruffinengo.agostina@tecnica7.edu.ar>
2024-11-26 17:46:31 -0300

    colores

--- src/navbar.js
+++ src/navbar.js
@@ -1,10 +1,10 @@
 const navbar = `
 <!-- menu head-->
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg" style="background-color: orange;">
 
         <div class="container-fluid">
 
-          <a class="navbar-brand" href="./index.html">Editorial</a>
+          <a class="navbar-brand text-white" href="./index.html">Editorial</a>
 
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
